2b032b9faa7e66add838c91e1ed17f1d94392d50
thiagobarros2 <thiagobarros95@gmail.com>
2023-03-18 10:24:37 -0300

    fix: alter typeOrmModule to get configs from data source

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,11 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';  
 import { UserModule } from './user/user.module';
-import { User } from './user/entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
+import { dataSourceOptions } from 'database/data-source';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User],
-      synchronize: false,
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     UserModule,
   ],
 })
