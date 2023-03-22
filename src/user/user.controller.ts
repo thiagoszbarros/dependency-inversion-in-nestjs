87a3e0a5f0df788a186fabe0a4622cf6b77c132e
thiagobarros2 <thiagobarros95@gmail.com>
2023-03-21 22:01:38 -0300

    chore: remove ApiResponse decorator from imports

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation} from '@nestjs/swagger';
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
