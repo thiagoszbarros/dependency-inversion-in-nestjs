d80ede5449e08fa6a4d68bc0419337aa47f993e8
thiagobarros2 <thiagobarros95@gmail.com>
2023-03-21 22:06:48 -0300

    chore: add CreateUserDto to imports

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { IUserRepo } from './interfaces/user-repo.interface';
 
