291299605c0939a74759700b4fe2bbc95249da1b
thiagobarros2 <thiagobarros95@gmail.com>
2023-03-22 01:28:31 -0300

    chore: import repository from typeorm

--- src/user/user.repository.spec.ts
+++ src/user/user.repository.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { UserRepo } from './user.repository';
 
