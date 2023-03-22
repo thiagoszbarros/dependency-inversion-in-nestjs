828168b7c73a494d7cd6d2df71893756288c5a74
thiagobarros2 <thiagobarros95@gmail.com>
2023-03-21 22:08:56 -0300

    feat: add abstract create into abstract classes

--- src/user/interfaces/user-repo.interface.ts
+++ src/user/interfaces/user-repo.interface.ts
@@ -4,4 +4,5 @@ import { User } from "../entities/user.entity";
 export abstract class IUserRepo {
     abstract findAll(): Promise<User[]>;
     abstract findOne(id: number): Promise<User>;
+    abstract create(CreateUserDto: CreateUserDto): Promise<User>;
 }
--- src/user/interfaces/user-service.interface.ts
+++ src/user/interfaces/user-service.interface.ts
@@ -1,6 +1,8 @@
+import { CreateUserDto } from "../dto/create-user.dto";
 import { User } from "../entities/user.entity";
 
 export abstract class IUserService {
     abstract findAll(): Promise<User[]>
     abstract findOne(id: number): Promise<User>
+    abstract create(CreateUserDto: CreateUserDto): Promise<User>
 }
