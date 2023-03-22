764c06708ba5ca6bd05f78461a85bce91e30e528
thiagobarros2 <thiagobarros95@gmail.com>
2023-03-22 01:12:49 -0300

    docs: add swagger ApiProperty decorator

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   lastName: string;
+
+  @ApiProperty()
   email: string;
+  
+  @ApiProperty()
   password: string;
 }
